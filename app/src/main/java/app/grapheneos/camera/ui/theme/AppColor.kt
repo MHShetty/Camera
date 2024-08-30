bc0a2f7e7e11aadf1ae6de71c8a525bacb27cf2c
MHShetty <shettymohitn@gmail.com>
2024-08-31 03:42:14 +0530

    Define a separate constants object for colors

---
+++ app/src/main/java/app/grapheneos/camera/ui/theme/AppColor.kt
@@ -0,0 +1,8 @@
+package app.grapheneos.camera.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColor {
+    val BackgroundColor = Color(0xff181c1f)
+    val AppBarColor = Color(0x77000000)
+}
